3f12147c0d35a5d9ffded019feaab5411d100225
Sonal Maharaj <sjmaharaj.business@gmail.com>
2022-08-07 03:04:48 +0200

    PokeDex home page

---
+++ components/Pokemon.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+
+const Pokemon = (props) => {
+  const pokeID = props.index + 1;
+  const hashID = "#" + ("000" + pokeID).slice(-3);
+  return (
+    <div className="grid grid-cols-1 justify-items-center p-5 relative">
+      <Image
+        width={150}
+        height={150}
+        layout="fixed"
+        src={`http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeID}.png`}
+        className=" bg-gray-200 rounded-t shadow-md"
+      />
+      <span className="absolute h-5 w-10 rounded-full bg-sky-500 top-2.5 right-0  text-xs font-semibold flex items-center justify-center">
+        {hashID}
+      </span>
+      <span className=" bg-slate-300 shadow-md text-black text-transform: capitalize w-[150px] text-xs font-semibold rounded-b h-7 flex items-center justify-center">
+        {props.monster.name}
+      </span>
+    </div>
+  );
+};
+
+export default Pokemon;
